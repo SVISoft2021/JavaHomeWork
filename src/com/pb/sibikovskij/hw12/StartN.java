534f0d341f9106b974a47afe5d86a47cc01f2130
SVISoft2021 <svisoft2015@gmail.com>
2022-01-07 20:18:48 +0200

    Создать в пакете hw12 консольное приложение "Телефонная книга" с применением лямбда функции и Stream API.
    Каждый элемент должен иметь:
    
        ФИО
        дата рождения
        телефоны (количество не ограничено)
        адрес
        дата и время редактирования
    
    Обеспечить следующий функционал:
    
        добавление элемента
        удаление элемента
        поиск элементов
        вывод всех записей с сортировкой по указанному полю (можно ограничиться двумя на выбор)
        редактирование элемента
        запись в файл всех данных
        загрузка из файла всех данных

--- src/com/pb/sibikovskij/hw12/StartN.java
+++ src/com/pb/sibikovskij/hw12/StartN.java
@@ -31,94 +31,91 @@ public class StartN {
         boolean runVuborInt = true;
         while (runVuborInt) {
             switch (VuborInt) {
-             case 0:
-                System.out.println("- Абоненти [добавлення записів] ---------------------");
-                phoneBookClient.NewClientTest();
-                System.out.println("");
-                StartProg();
-                System.out.print("\nВаш вибір -> ");
-                VuborInt = Vubor.nextInt();
-                break;
-             case 1:
-                System.out.println("- Абоненти [добавлення записів] ---------------------");
-                phoneBookClient.NewClient();
-                System.out.println("");
-                StartProg();
-                System.out.print("\nВаш вибір -> ");
-                VuborInt = Vubor.nextInt();
-                break;
-             case 2:
-                 System.out.println("- Абоненти [пошук записів] ---------------------");
-                 phoneBookClient.FindClientOne();
-                 System.out.println("");
-                 StartProg();
-                 System.out.print("\nВаш вибір -> ");
-                 VuborInt = Vubor.nextInt();
-                 break;
-             case 3:
-                 System.out.println("- Абоненти [редагування записів] ---------------------");
-                 phoneBookClient.EditClient();
-                 System.out.println("");
-                 StartProg();
-                 System.out.print("\nВаш вибір -> ");
-                 VuborInt = Vubor.nextInt();
-                 break;
-             case 4:
-                System.out.println("- Абоненти [перегляд записів] ---------------------");
-                phoneBookClient.ListClient();
-                System.out.println("");
-                StartProg();
-                System.out.print("\nВаш вибір -> ");
-                VuborInt = Vubor.nextInt();
-                break;
-             case 5:
-                 System.out.println("- Абоненти [сортування записів] ---------------------");
-                 phoneBookClient.SortClient();
-                 System.out.println("");
-                 StartProg();
-                 System.out.print("\nВаш вибір -> ");
-                 VuborInt = Vubor.nextInt();
-                 break;
-             case 6:
-                System.out.println("- Абонент [видалення записів] ---------------------");
-                phoneBookClient.DeleteClient();
-                System.out.println("");
-                StartProg();
-                System.out.print("\nВаш вибір -> ");
-                VuborInt = Vubor.nextInt();
-                break;
-             case 7:
-                 System.out.println("- Довідник [експорт у файл] ---------------------");
-                 phoneBookClient.SaveClient();
+                case 0:
+                    System.out.println("- Абоненти [добавлення записів] ---------------------");
+                    phoneBookClient.NewClientTest();
+                    System.out.println("");
+                    StartProg();
+                    System.out.print("\nВаш вибір -> ");
+                    VuborInt = Vubor.nextInt();
+                    break;
+                case 1:
+                    System.out.println("- Абоненти [добавлення записів] ---------------------");
+                    phoneBookClient.NewClient();
+                    System.out.println("");
+                    StartProg();
+                    System.out.print("\nВаш вибір -> ");
+                    VuborInt = Vubor.nextInt();
+                    break;
+                case 2:
+                    System.out.println("- Абоненти [пошук записів] ---------------------");
+                    phoneBookClient.FindClientOne();
+                    System.out.println("");
+                    StartProg();
+                    System.out.print("\nВаш вибір -> ");
+                    VuborInt = Vubor.nextInt();
+                    break;
+                case 3:
+                    System.out.println("- Абоненти [редагування записів] ---------------------");
+                    phoneBookClient.EditClient();
+                    System.out.println("");
+                    StartProg();
+                    System.out.print("\nВаш вибір -> ");
+                    VuborInt = Vubor.nextInt();
+                    break;
+                case 4:
+                    System.out.println("- Абоненти [перегляд записів] ---------------------");
+                    phoneBookClient.ListClient();
+                    System.out.println("");
+                    StartProg();
+                    System.out.print("\nВаш вибір -> ");
+                    VuborInt = Vubor.nextInt();
+                    break;
+                case 5:
+                    System.out.println("- Абоненти [сортування записів] ---------------------");
+                    phoneBookClient.SortClient();
+                    System.out.println("");
+                    StartProg();
+                    System.out.print("\nВаш вибір -> ");
+                    VuborInt = Vubor.nextInt();
+                    break;
+                case 6:
+                    System.out.println("- Абонент [видалення записів] ---------------------");
+                    phoneBookClient.DeleteClient();
+                    System.out.println("");
+                    StartProg();
+                    System.out.print("\nВаш вибір -> ");
+                    VuborInt = Vubor.nextInt();
+                    break;
+                case 7:
+                    System.out.println("- Довідник [експорт у файл] ---------------------");
+                    phoneBookClient.SaveClient();
 
-                 System.out.println("");
-                 StartProg();
-                 System.out.print("\nВаш вибір -> ");
-                 VuborInt = Vubor.nextInt();
-                 break;
-             case 8:
-                 System.out.println("- Довідник [імпорт із файлу] ---------------------");
-                 phoneBookClient.LoadClient();
+                    System.out.println("");
+                    StartProg();
+                    System.out.print("\nВаш вибір -> ");
+                    VuborInt = Vubor.nextInt();
+                    break;
+                case 8:
+                    System.out.println("- Довідник [імпорт із файлу] ---------------------");
+                    phoneBookClient.LoadClient();
 
-                 System.out.println("- Абоненти [перегляд записів] ---------------------");
-                 phoneBookClient.ListClient();
+                    System.out.println("- Абоненти [перегляд записів] ---------------------");
+                    phoneBookClient.ListClient();
 
-                 System.out.println("");
-                 StartProg();
-                 System.out.print("\nВаш вибір -> ");
-                 VuborInt = Vubor.nextInt();
-                 break;
-             case 9:
-                System.exit(0);
+                    System.out.println("");
+                    StartProg();
+                    System.out.print("\nВаш вибір -> ");
+                    VuborInt = Vubor.nextInt();
+                    break;
+                case 9:
+                    System.exit(0);
 
-             default:
-                System.out.print("Відсутнє меню. Повторіть вибір -> ");
-                VuborInt = Vubor.nextInt();
-                break;
-             }
+                default:
+                    System.out.print("Відсутнє меню. Повторіть вибір -> ");
+                    VuborInt = Vubor.nextInt();
+                    break;
+            }
         }
-
-
     }
-
 }
